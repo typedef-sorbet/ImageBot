590f0312833ddcf35af4e2e9559f8c82402e9c19
Spencer Warneke <warnespe001@gmail.com>
2022-07-31 21:22:57 -0700

    Added caching

--- config.py
+++ config.py
@@ -33,3 +33,12 @@ def discord_channel_id():
 def search_tokens():
     with open(join(_ROOT_PATH, "config.json"), "r") as config_file:
         return json.loads(config_file.read())["search_tokens"]
+
+def write_cache(cache_dict):
+    with open(join(_ROOT_PATH, "cache.json"), "w") as cache_file:
+        # TODO need to flush after dumps?
+        cache_file.write(json.dumps(cache_dict))
+
+def read_cache():
+    with open(join(_ROOT_PATH, "cache.json"), "r") as cache_file:
+        return json.loads(cache_file.read())
